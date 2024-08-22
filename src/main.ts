b2f863a891d7f3bdbea30e18a9a5c5a9e490f713
Turito Yuenan <74642745+TuritoYuenan@users.noreply.github.com>
2024-08-22 23:20:37 +0700

    update app to be aware of base url

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,36 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 
 import './style.css'
 import App from './App.vue'
 
+/** Pages for Blood donors */
+const donorRoutes: RouteRecordRaw = {
+	path: 'donor',
+	children: [
+		{ path: 'centers', component: () => import('./views/DonationCenters.vue') },
+		{ path: 'appoint', component: () => import('./views/DonorAppoint.vue') },
+		{ path: 'appointment', component: () => import('./views/DonorAppointment.vue') },
+	]
+}
+
+/** Pages for Medical staffs */
+const staffRoutes: RouteRecordRaw = {
+	path: 'staff',
+	children: [
+		{ path: 'login', component: () => import('./views/StaffLogin.vue') },
+		{ path: 'dashboard', component: () => import('./views/StaffDashboard.vue') },
+	]
+}
+
+/** Router for web app */
 const router = createRouter({
 	history: createWebHistory(),
-	routes: [
-		// Homepage
-		{ path: '/', component: () => import('./views/Homepage.vue') },
-
-		// Pages for Blood donors
-		{ path: '/donor/centers', component: () => import('./views/DonationCenters.vue') },
-		{ path: '/donor/appoint', component: () => import('./views/DonorAppoint.vue') },
-		{ path: '/donor/appointment', component: () => import('./views/DonorAppointment.vue') },
-
-		// Pages for Medical staffs
-		{ path: '/staff/login', component: () => import('./views/StaffLogin.vue') },
-		{ path: '/staff/dashboard', component: () => import('./views/StaffDashboard.vue') },
-	],
+	routes: [{
+		path: import.meta.env.BASE_URL,
+		children: [donorRoutes, staffRoutes],
+		component: () => import('./views/Homepage.vue'),
+	}],
 })
 
 createApp(App).use(router).mount('#app')
