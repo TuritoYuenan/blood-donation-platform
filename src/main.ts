0be6bbfa3f21b21e15e9fab5831801c045ac3762
Turito Yuenan <74642745+TuritoYuenan@users.noreply.github.com>
2024-08-23 19:18:43 +0700

    fix routing issue

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,25 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 import './style.css'
 import App from './App.vue'
 
-/** Pages for Blood donors */
-const donorRoutes: RouteRecordRaw = {
-	path: 'donor',
-	children: [
-		{ path: 'centers', component: () => import('./views/DonationCenters.vue') },
-		{ path: 'appoint', component: () => import('./views/DonorAppoint.vue') },
-		{ path: 'appointment', component: () => import('./views/DonorAppointment.vue') },
-	]
-}
-
-/** Pages for Medical staffs */
-const staffRoutes: RouteRecordRaw = {
-	path: 'staff',
-	children: [
-		{ path: 'login', component: () => import('./views/StaffLogin.vue') },
-		{ path: 'dashboard', component: () => import('./views/StaffDashboard.vue') },
-	]
-}
-
 /** Router for web app */
 const router = createRouter({
-	history: createWebHistory(),
-	routes: [{
-		path: import.meta.env.BASE_URL,
-		children: [donorRoutes, staffRoutes],
-		component: () => import('./views/Homepage.vue'),
-	}],
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		// Homepage
+		{ path: '/', component: () => import('./views/Homepage.vue') },
+
+		// Pages for Blood donors
+		{ path: '/donor/centers', component: () => import('./views/DonationCenters.vue') },
+		{ path: '/donor/appoint/:id', component: () => import('./views/DonorAppoint.vue') },
+		{ path: '/donor/appointment', component: () => import('./views/DonorAppointment.vue') },
+
+		// Pages for Medical staffs
+		{ path: '/staff/login', component: () => import('./views/StaffLogin.vue') },
+		{ path: '/staff/dashboard', component: () => import('./views/StaffDashboard.vue') },
+	],
 })
 
 createApp(App).use(router).mount('#app')
--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { RouterLink } from 'vue-router';
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
 	<h1>Blood Management System</h1>
-	<p><RouterLink to="/staff/dashboard">Enter as Staff</RouterLink></p>
-	<p><RouterLink to="/donor/centers">Enter as Donor</RouterLink></p>
+	<p><RouterLink to="/donor/centers">Enter as Blood Donor</RouterLink></p>
+	<p><RouterLink to="/staff/dashboard">Enter as Medical Staff</RouterLink></p>
 </template>
