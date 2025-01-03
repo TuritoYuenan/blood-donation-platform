d4d1fef1c0bc139378a497c79d80f4415e7d2653
Turito Yuenan <74642745+TuritoYuenan@users.noreply.github.com>
2025-01-03 23:42:28 +0700

    frontend: setup routing following sitemap

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,21 +1,67 @@
+import HomeView from '@/views/HomeView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
 	routes: [
+		// Home page
 		{
 			path: '/',
-			name: 'home',
 			component: HomeView,
 		},
+
+		// Info pages
 		{
 			path: '/about',
-			name: 'about',
-			// route level code-splitting
-			// this generates a separate chunk (About.[hash].js) for this route
-			// which is lazy-loaded when the route is visited.
 			component: () => import('../views/AboutView.vue'),
+		},
+		{
+			path: '/contact-us',
+			component: () => import('../views/ContactView.vue'),
+		},
+
+		// Personal pages
+		{
+			path: '/settings',
+			component: () => import('../views/SettingsView.vue'),
+		},
+		{
+			path: '/history',
+			component: () => import('../views/HistoryView.vue'),
+		},
+		{
+			path: '/profile',
+			component: () => import('../views/ProfileView.vue'),
+		},
+
+		// Donation pages
+		{
+			path: '/donation/centers',
+			component: () => import('../views/DCentersView.vue'),
+		},
+		{
+			path: '/donation/campaigns',
+			component: () => import('../views/DCampaignsView.vue'),
+		},
+
+		// Login & Signup
+		{
+			path: '/login',
+			component: () => import('../views/LoginView.vue'),
+		},
+		{
+			path: '/signup',
+			component: () => import('../views/SignupView.vue'),
+		},
+
+		// Footer pages
+		{
+			path: '/terms',
+			component: () => import('../views/TermsView.vue'),
+		},
+		{
+			path: '/privacy',
+			component: () => import('../views/PrivacyView.vue'),
 		},
 	],
 })
