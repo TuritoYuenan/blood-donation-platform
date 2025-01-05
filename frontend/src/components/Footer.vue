e03089bc476a61ba62a80ae85d83f2a2243b2b33
Turito Yuenan <74642745+TuritoYuenan@users.noreply.github.com>
2025-01-05 23:22:04 +0700

    frontend: start using npm bootstrap with embedded sass, prototype footer, remove devtool

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,8 +1,15 @@
-<script lang="ts">
+<script setup lang="ts">
 </script>
 
 <template>
-	<footer>
-		<p>Footer</p>
+	<footer class="container-fluid bg-dark text-bg-dark py-3 my-4">
+		<ul class="nav justify-content-center border-bottom pb-3 mb-3">
+			<li class="nav-item"><a href="#" class="nav-link px-2 text-bg-dark">Home</a></li>
+			<li class="nav-item"><a href="#" class="nav-link px-2 text-bg-dark">Features</a></li>
+			<li class="nav-item"><a href="#" class="nav-link px-2 text-bg-dark">Pricing</a></li>
+			<li class="nav-item"><a href="#" class="nav-link px-2 text-bg-dark">FAQs</a></li>
+			<li class="nav-item"><a href="#" class="nav-link px-2 text-bg-dark">About</a></li>
+		</ul>
+		<p class="text-center">&copy; 2025 HTTP Team by Containerised</p>
 	</footer>
 </template>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,8 @@
-import './assets/main.css'
+import './global.scss'
+import * as bootstrap from 'bootstrap'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+createApp(App).use(router).mount('#app')
