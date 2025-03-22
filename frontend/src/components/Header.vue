11436b4525bb0bd94290ed3226db4440175e5555
Turito Yuenan <74642745+TuritoYuenan@users.noreply.github.com>
2025-03-22 14:04:52 +1100

    frontend: replace `NavigationBar` with `Header`

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+	const navItems = [
+		{ name: 'Locations', link: '/location/centers' },
+		{ name: 'Link', link: '/' },
+		{ name: 'Sign in', link: '/signin' }
+	];
+</script>
+
+<template>
+	<header class="navbar navbar-expand-md p-3 bg-danger text-bg-danger rounded-3">
+		<RouterLink to="/" class="navbar-brand">Containerised BDP</RouterLink>
+
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+			data-bs-target="#collapsibleNavbar">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<div class="collapse navbar-collapse" id="collapsibleNavbar">
+			<nav class="navbar-nav ms-auto mb-2 mb-md-0">
+				<RouterLink v-for="item in navItems" :key="item.name" :to="item.link" class="nav-item nav-link">
+					{{ item.name }}
+				</RouterLink>
+			</nav>
+		</div>
+	</header>
+</template>
